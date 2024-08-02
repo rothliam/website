39f3c319d916060d6e6953b0ec34be121ee35e91
Liam Roth <lro142628@stud.gibb.ch>
2024-08-02 20:58:11 +0200

    abc

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8c8f6ccf2eedc77"; }
+/******/ 		__webpack_require__.h = function() { return "fe12d421913e3aeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/projekte.js
+++ pages/projekte.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Styles from './projekte.module.css'
 
 function projekte() {
   return (
-    <div>
-      How are you?
+    <>
+    <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css?family=Roboto:700');
+
+        body {
+          font-family:'Roboto';
+          background-color: #131417;  
+        }
+    `}
+    </style>
+    <div className={Styles.container}>
+    <div className={Styles.maintitle}>
+      <p>Hello you</p> 
     </div>
+    </div>
+    <div className={Styles.content}>
+      <a>der betende mann hat ein gebet ausgesprochen, als er sein gebet vollendet hatte, war er glücklich und zu frieden. daher möchte der mann auch dass er nach hause kehren darf. dede dedededa asd adasde ich habe krebs</a>
+    </div>
+    </>
   )
 }
 
